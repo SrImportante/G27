5857066cd11d1b15c5808e980d3c32ae5ee29c9d
bernatpontanglada <bernatpontanglada@enti.cat>
2017-03-24 10:28:23 +0100

    classe map

--- CoinRace/Map.cc
+++ CoinRace/Map.cc
@@ -1,28 +1,44 @@
 #include <iostream>
 #include <stdio.h>
 
+#include "Map.hh"
+
 
 class Map
 {
 private:
 	enum class Level { Easy, Medium, Hard };
-	char MapMatrix[15][30];
+	char MapMatrix[15][30];  //ha d'estar en el heap (new)
 
 public:
-	/*Map::Map() :
+	Map::Map()
+	{
+		for (int i = 0; i < 5 * static_cast<int>(Map::setLevel()); i++) 
+		{
+			for (int j = 0; i < 5 * static_cast<int>(Map::setLevel()) * 2; j++)
+			{
+				MapMatrix[i][j] = '*';
+				std::cout << MapMatrix[i][j];
+			}
 
-		//for (int i = 0; i < 5 * static_cast<int>(Level); i++) {}
-	{};
-	*/
+			std::cout << "\n";
+
+		}
+	};
+	
 	Level Map::setLevel() 
 	{
 		Level level;
 		int input;
-	std::cout << "Introdueix un nivell: \n\nFacil: 1\nNormal: 2\nDificil: 3" << std::endl;
+	std::cout << "Introdueix un nivell: \n\nFacil: 1\nNormal: 2\nDificil: 3\n\n" << std::endl;
 	std::cin >> input;
 	level = static_cast<Level>(input);
 	return level;
     }
 
-	//Map::modiMap(x, y, )
+	//x i y son la posicio del jugador, i element el char que ha de posar
+	void Map::modify(int &x, int &y, char &element)
+	{
+		MapMatrix[x][y] = element;
+	}
 };
--- CoinRace/Map.hh
+++ CoinRace/Map.hh
@@ -1,1 +1,12 @@
-#pragma once
+class Map
+{
+private:
+	enum class Level { Easy, Medium, Hard };
+	char MapMatrix[15][30];
+
+public:
+	Map::Map();
+	Level Map::setLevel();
+	void Map::modify(int &x, int &y, char &element);
+
+};
